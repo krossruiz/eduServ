36b70af2d55cb9d226074505135377d6e99d1906
Kross Ruiz <krossevruiz@gmail.com>
2019-11-30 15:24:39 -0800

    'db'

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-const express = require('express')
-const app = express()
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb+srv://educare:vaHt7ABJTExsyYEl@cluster0-k1sxg.mongodb.net/test?retryWrites=true&w=majority';
+let db;
+let dogCollection;
+
+mongo.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err, client) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  db = client.db('kennel');
+  dogCollection = db.collection('dogs');
+});
+
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 8080;
+
+app.get('/getDogs', (req, res) => {
+    if(dogCollection){
+        dogCollection.find().toArray((err, dogs) => {
+            res.send(dogs);
+        })
+    }
+    else{
+        res.send("Patiently waiting for getDogs implementation.");
+    }
+})
+
+app.post('/postDog', (req, res) => {
+   let {dog} = req;
+   if(dogCollection){
+    collection.insertOne(dog, (err, result) => {
+        res.send(result);
+    });
+   }
+   else{
+    res.send("Patiently waiting for postDog implementation.");
+   }
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
